ceffffb72a993eca46dbd9722dd3dfeddd056e8c
Nayema Nasir <nayema.nasir89@gmail.com>
2017-08-25 16:04:33 -0400

    Calculates the average of the values in an array.

--- exercise-4/src/com/nayema/Main.java
+++ exercise-4/src/com/nayema/Main.java
@@ -3,13 +3,13 @@ package com.nayema;
 public class Main {
 
     public static void main(String[] args) {
-        int[] numbers = {1, 2, 3, 4, 5, 6, 7};
+        int[] intArray = {1, 2, 3, 4, 5, 6, 7};
 
         int sum = 0;
-        for (int a : numbers) {
+        for (int a : intArray) {
             sum += a;
         }
-        int average = sum / numbers.length;
+        int average = sum / intArray.length;
 
         System.out.println("The Average of the values is: " + average);
     }

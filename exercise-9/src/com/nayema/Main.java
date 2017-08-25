6a65812ad3f342ea48af97658f62f9956c8510c6
Nayema Nasir <nayema.nasir89@gmail.com>
2017-08-25 16:52:47 -0400

    Creates shortcut to find min and max values of a given array.

--- exercise-9/src/com/nayema/Main.java
+++ exercise-9/src/com/nayema/Main.java
@@ -4,31 +4,13 @@ import java.util.Arrays;
 
 public class Main {
 
-    public static int printMin(int[] array) {
-        int min = array[0];
-        for (int i = 1; i < array.length; i++) {
-            if (array[i] < min) {
-                min = array[i];
-            }
-        }
-        return min;
-    }
-
-    public static int printMax(int[] array) {
-        int max = array[0];
-        for (int i = 1; i < array.length; i++) {
-            if (array[i] > max) {
-                max = array[i];
-            }
-        }
-        return max;
-    }
-
     public static void main(String[] args) {
-        int[] intArray = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+        int[] intArray = {7, 2, 3, 4, 5, 6, 1, 8, 9, 10};
 
         System.out.println("Array: " + Arrays.toString(intArray));
-        System.out.println("Minimum: " + printMin(intArray));
-        System.out.println("Maximum: " + printMax(intArray));
+        Arrays.sort(intArray);
+        System.out.println("Sorted Array: " + Arrays.toString(intArray));
+        System.out.println("Minimum: " + intArray[0]);
+        System.out.println("Maximum: " + intArray[intArray.length - 1]);
     }
 }

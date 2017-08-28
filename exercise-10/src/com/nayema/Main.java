1c5c81fe83b0a35efceb0870188f4cd292008981
Nayema Nasir <nayema.nasir89@gmail.com>
2017-08-28 14:28:37 -0400

    Corrects formulat for printing duplicates in an array.)

--- exercise-10/src/com/nayema/Main.java
+++ exercise-10/src/com/nayema/Main.java
@@ -13,8 +13,8 @@ public class Main {
 
     private static void printDuplicateInArray(int[] array) {
         System.out.print("Duplicate Element within array: ");
-        for (int i = 0; i < array.length; i++) {
-            for (int j = i + 1; j < array.length - 1; j++) {
+        for (int i = 0; i < array.length - 1; i++) {
+            for (int j = i + 1; j < array.length; j++) {
                 if ((array[i] == array[j]) && (i != j)) {
                     System.out.print(array[j] + " ");
                 }
